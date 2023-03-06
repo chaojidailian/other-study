7467ea16b13e8fdbcf62ced6575b5be313c8ac68
dailian <chaojidailian@gmail.com>
2023-03-06 16:06:10 +0800

    feat(.):数组扁平化实现方法

---
+++ js-handwriting-implementation/array-flat.js
@@ -0,0 +1,37 @@
+//任务：数组扁平化
+/**
+ * 主要需要处理的点：1. 如何迭代数组
+ *                  2. 调用数组方法是否会改变原数组
+ * 
+ */
+/**
+ * @description 1. 使用数组的reduce方法实现
+ * @param {Array} array 
+ * @returns {Array} 返回扁平化的数组
+ */
+function cjFlat(array) {
+  return array.reduce((pre, cur) => {
+    return Array.isArray(cur) ?
+      pre.concat(flat(cur)) : pre.concat(cur)
+  }, [])
+}
+
+/**
+ * @description 2. 迭代数组方法实现
+ * @param {Array} array 
+ * @returns {Array} 返回扁平化的数组
+ */
+function cjFlat(array) {
+  let result = []
+  array.forEach(item => {
+    if(Array.isArray(item)) {
+      result.push(...cjFlat(item))
+    }else {
+      result.push(item)
+    }
+  })
+  return result
+}
+
+// 3. 数组原生方法实现，传入一个deep深度变量
+Array.prototype.flat
